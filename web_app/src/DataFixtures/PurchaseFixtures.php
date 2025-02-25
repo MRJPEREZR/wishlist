2993b404fc135fb31c6db3e507f62b61eaae96cc
Julian PEREZ-RAMIREZ <julian.perez-ramirez@imt-atlantique.net>
2025-02-25 21:23:02 +0100

    adding records to user table

--- web_app/src/DataFixtures/PurchaseFixtures.php
+++ web_app/src/DataFixtures/PurchaseFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
-class ItemFixtures extends Fixture
+class PurchaseFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
